2e3514175b33209c070c97738f2ddb94d91d2fa2
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-29 12:53:38 +0500

    change form title

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,7 +4,7 @@ import "./Form.scss";
 export default function Form() {
   return (
     <form className="form">
-      <h2 className="form__title">Password Validation</h2>
+      <h2 className="form__title">Form Validation</h2>
       <Field type="password" placeholder="Password" isPassword={true} />
       <Field />
     </form>
